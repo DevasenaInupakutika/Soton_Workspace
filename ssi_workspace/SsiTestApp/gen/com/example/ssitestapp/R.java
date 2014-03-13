0f0ccf94fa5226e5885c402506ae6a87170c052b
DevasenaInupakutika <devasena.prasad@gmail.com>
2014-03-13 13:00:01 +0000

    UI layout + Splash screen Theme Update

--- ssi_workspace/SsiTestApp/gen/com/example/ssitestapp/R.java
+++ ssi_workspace/SsiTestApp/gen/com/example/ssitestapp/R.java
@@ -160,5 +160,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f080000;
+        public static final int CustomTheme=0x7f080002;
     }
 }
